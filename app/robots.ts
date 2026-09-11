7a4bd081c59ab1a355d651f0d1e8722cb5883f7d
kiddhu <2155559@qq.com>
2026-09-12 02:16:47 +0800

    seo: add robots and OAI search crawler rule

---
+++ app/robots.ts
@@ -0,0 +1,2 @@
+import type { MetadataRoute } from 'next';
+export default function robots():MetadataRoute.Robots{return{rules:[{userAgent:'*',allow:'/'},{userAgent:'OAI-SearchBot',allow:'/'}],sitemap:'https://seekapi.ai/sitemap.xml'};}
